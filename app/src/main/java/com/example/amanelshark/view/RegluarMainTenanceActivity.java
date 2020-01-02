d75d405f7017db7ffb6e8c4720f785f32a0d8fd5
qahmed89 <qahmed89@gmail.com>
2020-01-02 18:35:21 +0200

    dones

---
+++ app/src/main/java/com/example/amanelshark/view/RegluarMainTenanceActivity.java
@@ -0,0 +1,17 @@
+package com.example.amanelshark.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.amanelshark.R;
+
+public class RegluarMainTenanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regluarmain);
+
+    }
+}
